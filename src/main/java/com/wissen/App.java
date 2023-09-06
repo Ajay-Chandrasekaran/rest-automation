1ac33b954c7b38f805f2e9f0b4f0cec2e3ed0ec2
Ajay Chandrasekaran <ajaychandrasekaran99@gmail.com>
2023-09-06 13:56:12 +0530

    Init

---
+++ src/main/java/com/wissen/App.java
@@ -0,0 +1,9 @@
+package com.wissen;
+
+/**
+ * Test REST Endpoints
+ */
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/com/wissen/AppTest.java
@@ -0,0 +1,19 @@
+package com.wissen;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+// @Ignore
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void anotherTest() {
+        assertTrue(false);
+    }
+}
