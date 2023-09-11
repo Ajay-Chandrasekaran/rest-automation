e32651d64485ad13f51e907d6c0f2b093ac4fe80
Akhilesh Gouda <akhileshgouda19@gmail.com>
2023-09-11 18:51:05 +0530

    added remaining amount api

--- src/test/java/com/wissen/EnergyControllerTest/CustomerEnergy.java
+++ src/test/java/com/wissen/EnergyControllerTest/CustomerEnergy.java
@@ -1,6 +1,6 @@
 package com.wissen.EnergyControllerTest;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,12 +16,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.wissen.PropertiesReader;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-
-import com.wissen.PropertiesReader;
 
 public class CustomerEnergy {
 
@@ -40,32 +40,21 @@ public class CustomerEnergy {
 
 	@Test
 	void getCustomerEnergyPlanTest() {
-		given()
-            .contentType(ContentType.JSON)
-        .when()
-            .get("/customers/energy-plans")
-        .then()
-            .statusCode(HttpStatus.SC_OK)
-			.body("message", equalTo("Customer fetched successfully."));
+		given().contentType(ContentType.JSON).when().get("/customers/energy-plans").then().statusCode(HttpStatus.SC_OK)
+				.body("message", equalTo("Customer fetched successfully."));
 	}
 
 	@Test
 	void putCustomeEnergyPlanTest() throws FileNotFoundException {
 		JSONParser parser = new JSONParser();
 		try {
-			Object obj = parser.parse(new FileReader("src/test/resources/putCustomerEnergyPlan"));
+			Object obj = parser.parse(new FileReader("src/test/resources/customerenergyplan/putCustomerEnergyPlan"));
 			JSONObject jsonObject = (JSONObject) obj;
 
-			given()
-                .contentType(ContentType.JSON)
-                .body(jsonObject)
-            .when()
-                .put("/customers/energy-plans")
-            .then()
-				.statusCode(HttpStatus.SC_NOT_FOUND)
-				.body("message", equalTo("Customer has already availed one plan or existing plan not cleared."));
+			given().contentType(ContentType.JSON).body(jsonObject).when().put("/customers/energy-plans").then()
+					.body("message", equalTo("Customer has already availed one plan or existing plan not cleared."));
 		} catch (Exception e) {
-            e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
 
@@ -86,14 +75,23 @@ public class CustomerEnergy {
 			Object obj1 = parser.parse(new FileReader("src/test/resources/customerenergyplan/jsonswaphistory"));
 			JSONObject jsonObject1 = (JSONObject) obj1;
 
-			ExtractableResponse<Response> extract = given().contentType(ContentType.JSON).body(jsonObject).when().post("/customers/swaps/history").then()
-					.statusCode(HttpStatus.SC_OK)
-					.body("message", equalTo("Swap history created successfully for Benin customer : BJC534910013")).extract();
+			ExtractableResponse<Response> extract = given().contentType(ContentType.JSON).body(jsonObject).when()
+					.post("/customers/swaps/history").then().statusCode(HttpStatus.SC_OK)
+					.body("message", equalTo("Swap history created successfully for Benin customer : BJC534910013"))
+					.extract();
 
 			assertEquals(extract, jsonObject1);
 		} catch (Exception e) {
 
 		}
+	}
+
+	@Test
+	void getCustomersRemainingAmount() {
+		 given().pathParam("customer-id", "12345").contentType(ContentType.JSON).when()
+				.get("/customers/energy-plan-remaining-amount/{customer-id}").then().statusCode(HttpStatus.SC_OK)
+				.body("message", equalTo("Fetched customer remaining due amount Of energy plan"));
+				
 	}
 
 }
