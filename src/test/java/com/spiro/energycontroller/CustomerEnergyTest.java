f1d35f1c36ba971803942e0d822d718d33662ad4
Ajay Chandrasekaran <ajaychandrasekaran99@gmail.com>
2023-09-12 12:44:12 +0530

    make tests public

--- src/test/java/com/spiro/energycontroller/CustomerEnergyTest.java
+++ src/test/java/com/spiro/energycontroller/CustomerEnergyTest.java
@@ -23,12 +23,12 @@ import io.restassured.response.Response;
 
 import com.spiro.PropertiesReader;
 
-public class CustomerEnergy {
+public class CustomerEnergyTest {
 
 	@BeforeAll
 	public static void setup() throws IOException {
 		PropertiesReader propReader = PropertiesReader.getReader();
-		propReader.useDev();
+		propReader.useDevEnv();
 		RestAssured.baseURI = propReader.getHost();
 		RestAssured.port = propReader.getPort();
 	}
@@ -38,16 +38,16 @@ public class CustomerEnergy {
 		RestAssured.reset();
 	}
     @Test
-	void getCustomerEnergyPlanTest() {
+	public void getCustomerEnergyPlanTest() {
 		given().contentType(ContentType.JSON).when().get("/customers/energy-plans").then().statusCode(HttpStatus.SC_OK)
 				.body("message", equalTo("Customer fetched successfully."));
 	}
 
 	@Test
-	void putCustomeEnergyPlanTest() throws FileNotFoundException {
+	public void putCustomeEnergyPlanTest() throws FileNotFoundException {
 		JSONParser parser = new JSONParser();
 		try {
-			Object obj = parser.parse(new FileReader("src/test/resources/customerenergyplan/putCustomerEnergyPla.json"));
+			Object obj = parser.parse(new FileReader("src/test/resources/customerenergyplan/putCustomerEnergyPlan.json"));
 			JSONObject jsonObject = (JSONObject) obj;
 
 			given().contentType(ContentType.JSON).body(jsonObject).when().put("/customers/energy-plans").then()
@@ -58,14 +58,14 @@ public class CustomerEnergy {
 	}
 
 	@Test
-	void getCustomerByIdTest() {
+	public void getCustomerByIdTest() {
 		given().pathParam("customer-id", "12345").contentType(ContentType.JSON).when()
 				.get("/customers/{customer-id}/energy-plans").then().statusCode(HttpStatus.SC_OK);
 
 	}
 
 	@Test
-	void postSwapHistory() throws FileNotFoundException, IOException, ParseException {
+	public void postSwapHistory() throws FileNotFoundException, IOException, ParseException {
 		JSONParser parser = new JSONParser();
 		try {
 			Object obj = parser.parse(new FileReader("src/test/resources/SwapHistory"));
@@ -86,7 +86,7 @@ public class CustomerEnergy {
 	}
 
 	@Test
-	void getCustomersRemainingAmount() {
+	public void getCustomersRemainingAmount() {
 		given().pathParam("customer-id", "BJC534910013").contentType(ContentType.JSON).when()
 				.get("/customers/energy-plan-remaining-amount/{customer-id}").then().statusCode(HttpStatus.SC_OK)
 				.body("message", equalTo("Fetched customer remaining due amount Of energy plan"));
@@ -94,7 +94,7 @@ public class CustomerEnergy {
 	}
 
 	@Test
-	void postCustomerSwapHistoryTest() throws FileNotFoundException, IOException, ParseException {
+	public void postCustomerSwapHistoryTest() throws FileNotFoundException, IOException, ParseException {
 
 		JSONParser parser = new JSONParser();
 
@@ -106,7 +106,7 @@ public class CustomerEnergy {
 	}
 
 	@Test
-	void getCustomerSwapHistoryTest() {
+	public void getCustomerSwapHistoryTest() {
 		given().pathParam("customer-id", "BJC534910013").contentType(ContentType.JSON).when()
 		.get("/customers/payment-history/{customer-id}").then().statusCode(HttpStatus.SC_OK)
 		.body("message", equalTo("Fetched energy plan payment history."));
